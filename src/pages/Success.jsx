ae6b7a005ecaa0ca70bcaceb404978f39ce296e9
RamDev <daudalaramadan2@gmail.com>
2025-01-12 14:43:13 +0000

    update Success component message to indicate design work needed

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -9,7 +9,7 @@ const Success = () => {
   }
   return (
     <>
-     <div>Success</div>
+     <div>Success got to work on the design for this </div>
      <button className="gohome" onClick={Gohome}>Home</button></>
   )
 }
